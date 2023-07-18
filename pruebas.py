44b2ba92e1fb0e905ae846bd1cd1aa3f078bb17d
Jairo <jn.gomezm1@unaindes.edu.co>
2023-07-18 11:28:55 -0500

    Ensayos

--- pruebas.py
+++ pruebas.py
@@ -1,17 +1,4 @@
-
-list1 = [1,2,4]
-list2 = [1,2,3,4]
-n =len(list1)
-m = 4
-pointer1 = n - 1
-pointer2 = m - 1
-pointerU = n + m - 1
-
-list1.extend(len(list2)*[0])
-
-
-print(list1)
-
+#Pelotudo!!!!!!!!!!
 
 
 
